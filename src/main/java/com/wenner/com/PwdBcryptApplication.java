322952ae93a062065a722407341bc706e4793064
wenner-sanner <wennersanner@hotmail.com>
2018-03-14 18:36:59 -0300

    Teste de segurança de senha com BCrypt

--- src/main/java/com/wenner/com/PwdBcryptApplication.java
+++ src/main/java/com/wenner/com/PwdBcryptApplication.java
@@ -1,12 +1,30 @@
 package com.wenner.com;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.wenner.com.utils.SenhaUtils;
 
 @SpringBootApplication
 public class PwdBcryptApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PwdBcryptApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded ==> " + senhaEncoded);
+			
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente: " + senhaEncoded);
+			
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+		};
 	}
 }
