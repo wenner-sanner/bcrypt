d2ebe9151720b3878fc17bbb4b6a73f82d0fba6d
wenner-sanner <wennersanner@hotmail.com>
2018-03-14 10:30:08 -0300

    Iniciando o projeto

---
+++ src/main/java/com/wenner/com/PwdBcryptApplication.java
@@ -0,0 +1,12 @@
+package com.wenner.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwdBcryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwdBcryptApplication.class, args);
+	}
+}
